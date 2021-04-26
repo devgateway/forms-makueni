8e4b4b95947823caa1ef034ba6b4b908b611e9f1
Octavian Ciubotaru <octavian@ciubotaru.net>
2021-04-26 15:18:16 +0300

    md5

--- persistence/src/main/java/org/devgateway/toolkit/persistence/dao/FileMetadata.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/dao/FileMetadata.java
@@ -50,6 +50,8 @@ public class FileMetadata extends AbstractAuditableEntity {
 
     private long size;
 
+    private String md5;
+
     @Override
     public String toString() {
         return name;
@@ -93,6 +95,14 @@ public class FileMetadata extends AbstractAuditableEntity {
 
     public void setSize(final long size) {
         this.size = size;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
     }
 
     @Override
